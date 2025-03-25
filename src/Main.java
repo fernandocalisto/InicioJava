c864230b9f9a354c2d1afd5269bac5502e418a3e
Fernando Calisto <fernandocalisto27@gmail.com>
2025-03-25 17:24:38 -0300

    Estudo de Java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        double nota = 8.1;
+        System.out.println("A sua nota pro filme é: " + nota);
+        boolean incluidoNoPlano = true;
+        double media = (9.8 + 6.3 + 8) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme de Carros Fodas e Ação";
+        System.out.println(sinopse);
+        //comentário muito útil;
+        }
+    }
